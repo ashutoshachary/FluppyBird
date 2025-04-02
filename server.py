3d11c3da15141a2cdfd3aca20f5f3c6bac782399
ashutoshachary <111655759+ashutoshachary@users.noreply.github.com>
2025-04-02 13:09:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'html5-game.html')
 
 @app.route('/<path:path>')
 def serve_files(path):
